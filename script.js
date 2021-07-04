adb5c9b3062a2f1afe3177f0bafd76406cd89c76
leoor22 <leonelrivas.rl@gmail.com>
2021-07-04 18:35:13 -0300

    script of the game complete

--- script.js
+++ script.js
@@ -3,35 +3,84 @@
 
 
 
-let computerplay = function(){
-    let randomitem = Math.floor(Math.random()*3) + 1;
-    if (randomitem == 1) {
-        return  ("rock") ;
-    }   else if (randomitem ==2) {
-        return  ("paper");
-    }
-   else {
-    return ("siccors")
-   }
-   const computerSelection = computerplay()
-}
 
 
+let game = function(){
 
-let playRound = function(){
-    if (playerSelection == computerSelection){
-         alert("Tie");
-         computerplay()
-    } else if (((playerSelection=="rock") && (computerSelection=="paper") ) || (((playerSelection=="paper") && (computerSelection=="siccors"))) || (((playerSelection=="siccors") && (computerSelection=="rock")))){
-         alert("Lose");
-         computerplay()
-    }
+    let userScore = 0
+    let computerScore = 0
 
-     else if (((playerSelection=="paper") && (computerSelection=="rock")) ||  (((playerSelection=="siccors") && (computerSelection=="paper")))  || ((playerSelection=="rock") && (computerSelection=="siccors"))){
-             alert("win");  
-             computerplay()
-    }
-}
+
+  let playRound = function(){
+
     
+
+    let computerplay = function(){
+        let randomitem = Math.floor(Math.random()*3) + 1;
+        if (randomitem == 1) {
+            return  ("rock") ;
+        }   else if (randomitem ==2) {
+            return  ("paper");
+        }
+       else {
+        return ("scissors")
+       }
+    }
+    computerplay();
+
+    const playerSelection= prompt("Choose rock, paper or scissors").toLowerCase()
     const computerSelection = computerplay();
-    const playerSelection="rock";
+
+
+    if (playerSelection == computerSelection){
+
+        userScore += 1 ; computerScore += 1 ;
+        
+        console.log ("Tie. " + playerSelection + " equals " + computerSelection)
+        
+        console.log ("User score = " + userScore + " --- Computer score = " + computerScore)
+
+    } else if (((playerSelection=="rock") && (computerSelection=="paper") )      ||
+              (((playerSelection=="paper") && (computerSelection=="scissors")))  || 
+              (((playerSelection=="scissors") && (computerSelection=="rock")))){
+
+
+        userScore += 0 ; computerScore += 1 ;
+        
+        console.log ("You lose! " + computerSelection + " beats " + playerSelection)
+        
+        
+        console.log ("User score = " + userScore + " --- Computer score = " + computerScore)
+
+    }
+
+     else {
+        
+        userScore += 1 ; computerScore += 0 ;
+        
+        console.log ("You win! " +  playerSelection + " beats " + computerSelection)
+        
+        console.log ("User score = " + userScore + " --- Computer score = " + computerScore)
+
+    }
+  }
+ playRound();
+ playRound();
+ playRound();
+ playRound();
+ playRound();
+
+ if (userScore == computerScore) {
+     console.log ("The result of the game was a draw.")
+ }
+ else if (userScore > computerScore){
+     console.log ("User wins !")
+ }
+
+
+ else {
+     console.log ("Computer wins !")
+ }
+
+ 
+}
